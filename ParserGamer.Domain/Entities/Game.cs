da9b87bd43dbbaf1c07cab952828c802c905ca97
Bruno Santos <brunoj.santos91@gmail.com>
2017-10-13 17:04:43 -0300

    Criado as entidades que irão representar os devidos cenários do jogo

---
+++ ParserGamer.Domain/Entities/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserGamer.Domain.Entities
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public DateTime TimeStart { get; set; }
+        public DateTime TimeEnd { get; set; }
+        public Information Information { get; set; }
+        public int TotalKills { get; set; }
+    }
+}
---
+++ ParserGamer.Domain/Entities/Information.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserGamer.Domain.Entities
+{
+    public class Information
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int IdPlayer1 { get; set; }
+        public virtual Player Player1 { get; set; }
+        public int IdPlayer2 { get; set; }
+        public virtual Player Player2 { get; set; }
+    }
+}
